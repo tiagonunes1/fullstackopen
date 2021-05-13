a916b42a9276ae1e4ff6254faca0b3d43581c121
Tiago Nunes <44710976+tiagonunes1@users.noreply.github.com>
2021-05-13 09:56:26 +0100

    Update App.js

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -14,24 +14,38 @@ const Button = ({click, text}) => {
   )
 }
 
-const Stats = () =>{
+const Stats = ({good,neutral,bad}) =>{
+  let perc = ''
+  
+  if (feedback === 'positive') perc ='%'
+  if (feedback !== 'positive') perc =''
+  
+  const all = good + neutral + bad
+  const positive = ( good / all ) * 100 
+  const average = good * 1 + neutral * 0 + bad * -1
+  if (good > 0 || neutral > 0 || bad > 0) {
   return (
-    <div>
-      <h2>
-        statistics
-      </h2>
-    </div>
+      <div>
+        <h2>
+          statistics
+        </h2>
+      <Result count={good} feedback="good" />
+      <Result count={neutral} feedback="neutral" />
+      <Result count={bad} feedback="bad" />
+      <Result count={all} feedback="all" />
+      <Result count={average} feedback="average" />
+      <Result count={positive} feedback="positive" />
+      </div>
+    )
+  }
+  return(
+    <p>No feedback yet.</p>
   )
 }
 
-const Result = ({feedback, count}) =>{
-  let perc = ''
-
-  if (feedback === 'positive') perc ='%'
-  if (feedback !== 'positive') perc =''
-
+const Result = ({feedback, value}) =>{
   return [
-  <p>{feedback}: {count}{perc}</p>
+  <p>{feedback}: {count}</p>
   ]
 }
 
@@ -41,29 +55,16 @@ function App() {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-  const [allClicks, setAll] = useState(0)
-  const [average, setAverage] = useState(0)
-  const [positive, setPositive] = useState(0)
 
   const handleGood = () => {
-    setAll(allClicks + 1 )
     setGood(good + 1)
-    setPositive(positive + 1)
-    setPositive((positive/allClicks)*100)
-    console.log(allClicks)
-    console.log(positive)
   }
   const handleNeutral = () => {
-    setAll(allClicks + 1)
     setNeutral(neutral + 1)
   }
   const handleBad = () => {
-    setAll(allClicks + 1)
     setBad(bad + 1)
     
-  }
-  const handlePositive = () => {
-    setPositive(positive / allClicks * 100)
   }
 
   return (
@@ -72,14 +73,7 @@ function App() {
     <Button text="good" click={handleGood}/>
     <Button text="neutral" click={handleNeutral}/>
     <Button text="bad" click={handleBad}/>
-    <Stats />
-    <Result feedback="good" count={good}/>
-    <Result feedback="neutral" count={neutral}/>
-    <Result feedback="bad" count={bad}/>
-    <Result feedback="all" count={allClicks}/>
-    <Result feedback="average"/>
-    <Result feedback="positive" count={positive}/>
-
+    <Stats good = {good} neutral = {neutral} bad = {bad} />
     </div>
   )
 }
