c219388279eb07df1d4e013b4548125a14831ba9
Tiago Nunes <44710976+tiagonunes1@users.noreply.github.com>
2021-05-14 17:50:49 +0100

    Update Course.js

--- part2/courseinfo/src/components/Course.js
+++ part2/courseinfo/src/components/Course.js
@@ -1,12 +1,40 @@
 import React from 'react'
 
-const Course = ({ courses,title }) => {
+const Course = ({ course }) => {
     return (
     <div>
-      <h1>{title}</h1>
-       {courses}
+      <Header name={course.name}/>
+      <Content parts={course.parts}/>
+      <Total parts={course.parts}/>
       </div>
       )
+}
+
+const Header = ({ name }) => {
+  return <h1>{name}</h1>
+}
+
+const Content = ({ parts }) => {
+ return(
+  <div>
+  {parts.map((cont) => (
+    <Part key={cont.id} cont={cont}/>
+  ))}
+  </div>
+ )
+}
+
+const Part = ({ cont }) =>{
+  return (
+  <p>{cont.name}: {cont.exercises}</p>
+  )
+}
+
+const Total = ({ parts }) => {
+  const soma = parts.reduce((total, valor) => total + valor.exercises,0);
+  return(
+    <p><strong>total of <u>{soma}</u> exercises </strong></p>
+  )
 }
 
 export default Course
