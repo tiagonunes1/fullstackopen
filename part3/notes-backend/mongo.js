47758c0483f7d5b2e974c30c6e8dfae03d11e21e
Tiago Nunes <44710976+tiagonunes1@users.noreply.github.com>
2021-06-01 00:42:50 +0100

    Add files via upload

---
+++ part3/notes-backend/mongo.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3) {
+  console.log('Please provide the password as an argument: node mongo.js <password>')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+
+  const url=`mongodb+srv://fullstack:${password}@cluster0.iugj5.mongodb.net/note-app?retryWrites=true&w=majority`
+
+
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+
+const noteSchema = new mongoose.Schema({
+  content: String,
+  date: Date,
+  important: Boolean,
+})
+
+const Note = mongoose.model('Note', noteSchema)
+
+const note = new Note({
+  content: 'HTML is hard asf',
+  date: new Date(),
+  important: true,
+})
+
+Note.find({}).then(result => {
+    result.forEach(note => {
+        console.log(note)
+    }) 
+    mongoose.connection.close()
+})
+
+
+note.save().then(result => {
+    console.log('note saved!')
+    mongoose.connection.close()
+  })
