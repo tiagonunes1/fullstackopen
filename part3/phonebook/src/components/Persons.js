33ef020031ca48dbba6e7ad07f389b1d763962f3
Tiago Nunes <44710976+tiagonunes1@users.noreply.github.com>
2021-06-01 13:50:01 +0100

    Delete part3/phonebook directory

--- part3/phonebook/src/components/Persons.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-
-const Persons = ({ person,onClick }) =>{
-    return(
-    <li>{person.name} - {person.number} <button onClick={onClick}>delete</button></li>
-    )
-}
-export default Persons
